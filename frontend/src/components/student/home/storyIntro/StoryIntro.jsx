1ebcf9ded0412a9b5f94d1c09f62f46b4eac9731
Kaori <studiokaori.com>
2020-12-10 21:37:55 +0100

    Add anime

--- frontend/src/components/student/home/storyIntro/StoryIntro.jsx
+++ frontend/src/components/student/home/storyIntro/StoryIntro.jsx
@@ -3,21 +3,18 @@ import "../../../../css/student/storyIntro/storyIntro.css";
 export default function StoryIntro() {
   return (
     <div className="storyIntro">
-      <div className="storyIntroElement fadeIn">
-        This is the story of parallel world...
-      </div>
-      <div className="storyIntroElement fadeIn">AD 2080</div>
+      <div className="storyIntroElement fadeIn">AD 3080</div>
       <div className="storyIntroElement fadeIn">
         Climate change hit the world
       </div>
       <div className="storyIntroElement fadeIn">Many cities were lost</div>
       <div className="storyIntroElement fadeIn">Your mission is</div>
-      <div className="storyIntroElement storyIntroLast">
+      <div className="storyIntroElement fadeIn">
         <div class="animate__animated animate__bounce animate__delay-3s">
           Find the lost cities
         </div>
         <div>and...</div>
-        <div>Hunt the Monsters!!</div>
+        <div>Hunt Monsters!!</div>
       </div>
     </div>
   );
